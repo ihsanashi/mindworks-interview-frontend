9119e4ca1e6f3ab09e3e14ff3c75194f6aef3522
Ahmad Ihsan <ihsanashi@gmail.com>
2021-07-19 09:07:36 +0800

    add rel attribute for jsonplaceholder link

--- pages/index.js
+++ pages/index.js
@@ -17,14 +17,15 @@ export default function Home({ data }) {
         <div className='container'>
           <section className='max-w-lg mx-auto'>
             <div className='text-center'>
-              <h1 className='text-4xl mb-5 text-transparent font-semibold bg-gradient-to-r bg-clip-text from-red-500 to-purple-500'>
+              <h1 className='mb-5 text-4xl font-semibold text-transparent bg-gradient-to-r bg-clip-text from-red-500 to-purple-500'>
                 Displaying all posts
               </h1>
-              <h5 className='text-gray-700 text-lg'>
+              <h5 className='text-lg text-gray-700'>
                 Content retrieved from{' '}
                 <a
-                  className='underline inline-block text-purple-400 hover:text-purple-600 transition duration-300 ease-in-out'
+                  className='inline-block text-purple-400 underline transition duration-300 ease-in-out hover:text-purple-600'
                   target='_blank'
+                  rel='noreferrer'
                   href='https://jsonplaceholder.typicode.com/'
                 >
                   JSON Placeholder
@@ -32,12 +33,12 @@ export default function Home({ data }) {
               </h5>
             </div>
           </section>
-          <section className='max-w-max mx-auto mt-8'>
-            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-5'>
+          <section className='mx-auto mt-8 max-w-max'>
+            <div className='grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6'>
               {data.map((post) => (
                 <Link href={`/posts/${post.id}`} key={post.id}>
                   <a className='inline-grid'>
-                    <div className='border rounded-md border-purple-200 p-5 group hover:border-transparent hover:shadow-lg hover:bg-pink-500 transition duration-300 ease-in-out'>
+                    <div className='p-5 transition duration-300 ease-in-out border border-purple-200 rounded-md group hover:border-transparent hover:shadow-lg hover:bg-pink-500'>
                       <small className='bg-pink-200 text-pink-500 py-0.5 px-1.5 text-center inline-block rounded-md'>
                         #{post.id}
                       </small>
